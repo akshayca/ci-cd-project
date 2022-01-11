3083f8311cda7907898248858b11bac4aaeb53bf
akshayca <akshay.ca.07@gmail.com>
2022-01-11 13:54:48 +0530

    Added groovy script for Jenkins

--- script.groovy
+++ script.groovy
@@ -6,13 +6,14 @@ def buildJar(){
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    // bat "docker build -t ${DOCKER_REPO}:${IMAGE_NAME} ."
-    // bat "echo $PASS | docker login -u $USER --password-stdin ${DOCKER_REPO_SERVER}"
-    // bat   93333 "docker push ${DOCKER_REPO}:${IMAGE_NAME}"
-    bat "docker build -t akshayca23/ci-cd-project:jma-1.0 ."
-    bat "docker login -u %USER% -p %PASS%"
-    //bat "echo %PASS% | docker login -u %USER% --password-stdin"  
-    bat "docker push akshayca23/ci-cd-project:jma-1.0"
+        // bat "docker build -t ${DOCKER_REPO}:${IMAGE_NAME} ."
+        // bat "echo $PASS | docker login -u $USER --password-stdin ${DOCKER_REPO_SERVER}"
+        // bat   93333 "docker push ${DOCKER_REPO}:${IMAGE_NAME}"
+        bat "docker build -t akshayca23/ci-cd-project:jma-1.0 ."
+        bat "docker login -u %USER% -p %PASS%"
+        //bat "echo %PASS% | docker login -u %USER% --password-stdin"  
+        bat "docker push akshayca23/ci-cd-project:jma-1.0"
+    }
 }
 
 def testApp(){
